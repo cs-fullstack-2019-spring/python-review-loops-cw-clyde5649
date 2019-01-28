1227c9bd3878ed095b96314b29f9ad6b09678fa9
clyde5649 <clyde.bledsoe@yahoo.com>
2019-01-28 15:51:29 -0600

    yo

---
+++ cw 1.py
@@ -0,0 +1,45 @@
+def main():
+    def exercise
+
+        if  i==1 in range(0,7):
+            (i == 3) or (i == 6):
+               continue
+
+
+
+
+///////////////
+def main():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ inclass.py
@@ -0,0 +1,13 @@
+
+number1 = 2
+number2 = 3
+
+print("%d + %d = %d" % (number1, number2, number1+number2))
+print(f"{number1} + {number2} = {number1+number2}")"
+
+import random
+randomNumber = random.randint(0,9)
+print(randomNumber)
+
+
+
